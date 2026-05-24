83958273dd93491a7405773d9a1c7c39c00cf2cc
la094 <devowl094@gmail.com>
2026-05-24 21:38:49 +0900

    feat: 공통 페이지네이션 모델 구현

--- readme.md
+++ readme.md
@@ -1,8 +1,8 @@
 TODO:
 
 - 시큐리티 필터 레이어에서 JWT 만료로 인한 접근 실패시 클라가 식별 가능한 Error Key를 Body에 넣어주기
+- Sort model 구현
 - Swagger 적용하기
-- 공통 페이지네이션(오프셋, 커서) 처리하기
 - QueryDSL 기반 동적쿼리 Search 처리하기
 - RestClient로 외부 API 호출 방식 확인하기
 - 구조적 로그 구조 잡고 로거 적용하기 (로깅시 민감정보 마스킹 방법 찾아보기)
--- src/main/kotlin/me/blueblack094/springtest/core/domain/member/MemberRepo.kt
+++ src/main/kotlin/me/blueblack094/springtest/core/domain/member/MemberRepo.kt
@@ -1,8 +1,12 @@
 package me.blueblack094.springtest.core.domain.member
 
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import java.util.*
 
 interface MemberRepo : JpaRepository<Member, UUID> {
     fun findByEmail(email: String): Member?
+
+    fun findAllByEmail(email: String, page: Pageable): Page<Member>
 }
