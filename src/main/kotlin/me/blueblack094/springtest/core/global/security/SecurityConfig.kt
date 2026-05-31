849cbbd74dc581ebed00e23300eb52cd934a05b2
la094 <devowl094@gmail.com>
2026-05-31 23:47:09 +0900

    feat(Security): Public Path에서는 JWT 검증 생략

--- src/main/kotlin/me/blueblack094/springtest/core/global/security/SecurityConfig.kt
+++ src/main/kotlin/me/blueblack094/springtest/core/global/security/SecurityConfig.kt
@@ -34,17 +34,6 @@ class SecurityConfig(
 
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
-        val publicPaths = arrayOf(
-            // 헬스체크 경로
-            "/",
-            // 디테일한 에러 표시 (개발 단계)
-            "/error",
-            // H2 콘솔 허용 (개발 단계)
-            "/h2-console/**",
-            "/signup",
-            "/signin",
-        )
-
         return http.apply {
             csrf { it.disable() }
             headers {
@@ -57,7 +46,7 @@ class SecurityConfig(
             }
             authorizeHttpRequests {
                 it
-                    .requestMatchers(*publicPaths).permitAll()
+                    .requestMatchers(*SecurityPaths.PUBLIC_PATHS).permitAll()
                     .anyRequest().authenticated()
             }
             addFilterBefore(
